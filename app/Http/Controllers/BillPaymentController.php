18b0c3748d8bfa8ac5433830b3f6bacbe482e860
Mastarstroke <mastarstroke@gmail.com>
2024-11-28 21:45:41 +0100

    Log Airtime Purchase

--- app/Http/Controllers/BillPaymentController.php
+++ app/Http/Controllers/BillPaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Models\AirtimePurchase;
 use Illuminate\Support\Facades\DB;
 
 class BillPaymentController extends Controller
@@ -60,6 +61,13 @@ class BillPaymentController extends Controller
     
         try {
             $wallet->debit($validated['amount'], 'Airtime purchase');
+
+            // Log the purchase in the AirtimePurchase table
+            AirtimePurchase::create([
+                'user_id' => auth()->id(),
+                'phone_number' => $validated['phone_number'],
+                'amount' => $validated['amount'],
+            ]);
     
             return response()->json(['message' => 'Airtime purchased successfully'], 200);
         } catch (\Exception $e) {
